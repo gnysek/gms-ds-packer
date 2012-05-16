3063b833d92f47826774e1c99ebaecfbd4be4a7b
gnysek <gnysek@gmail.com>
2012-05-16 23:54:06 +0100

    initial commit, app isn't working yet, DS map/list packager is working

---
+++ DS Packer/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace DS_Packer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "asdasd",
+            "1we"}, -1);
+			System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("eeee");
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.cKey = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.cValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.statusBar1 = new System.Windows.Forms.StatusBar();
+			this.statusBarPanel1 = new System.Windows.Forms.StatusBarPanel();
+			this.statusBarPanel2 = new System.Windows.Forms.StatusBarPanel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panelTop = new System.Windows.Forms.Panel();
+			this.splitButton1 = new wyDay.Controls.SplitButton();
+			this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+			this.menuItem1 = new System.Windows.Forms.MenuItem();
+			this.menuItem2 = new System.Windows.Forms.MenuItem();
+			this.menuItem3 = new System.Windows.Forms.MenuItem();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.btnImportCSV = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.btnOptions = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.statusBarPanel1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.statusBarPanel2)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.panelTop.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cKey,
+            this.cValue});
+			this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listView1.GridLines = true;
+			this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+			this.listView1.LabelEdit = true;
+			this.listView1.Location = new System.Drawing.Point(0, 0);
+			this.listView1.MultiSelect = false;
+			this.listView1.Name = "listView1";
+			this.listView1.ShowGroups = false;
+			this.listView1.Size = new System.Drawing.Size(736, 211);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// cKey
+			// 
+			this.cKey.Text = "Key";
+			this.cKey.Width = 100;
+			// 
+			// cValue
+			// 
+			this.cValue.Text = "Value";
+			this.cValue.Width = 100;
+			// 
+			// statusBar1
+			// 
+			this.statusBar1.Location = new System.Drawing.Point(0, 411);
+			this.statusBar1.Name = "statusBar1";
+			this.statusBar1.Panels.AddRange(new System.Windows.Forms.StatusBarPanel[] {
+            this.statusBarPanel1,
+            this.statusBarPanel2});
+			this.statusBar1.ShowPanels = true;
+			this.statusBar1.Size = new System.Drawing.Size(736, 22);
+			this.statusBar1.SizingGrip = false;
+			this.statusBar1.TabIndex = 1;
+			this.statusBar1.Text = "statusBar1";
+			// 
+			// statusBarPanel1
+			// 
+			this.statusBarPanel1.Name = "statusBarPanel1";
+			this.statusBarPanel1.Text = "statusBarPanel1";
+			// 
+			// statusBarPanel2
+			// 
+			this.statusBarPanel2.Name = "statusBarPanel2";
+			this.statusBarPanel2.Text = "statusBarPanel2";
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.listView1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 200);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(736, 211);
+			this.panel1.TabIndex = 2;
+			// 
+			// panelTop
+			// 
+			this.panelTop.Controls.Add(this.btnOptions);
+			this.panelTop.Controls.Add(this.groupBox1);
+			this.panelTop.Controls.Add(this.splitButton1);
+			this.panelTop.Controls.Add(this.button3);
+			this.panelTop.Controls.Add(this.button2);
+			this.panelTop.Controls.Add(this.btnImportCSV);
+			this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panelTop.Location = new System.Drawing.Point(0, 0);
+			this.panelTop.Name = "panelTop";
+			this.panelTop.Size = new System.Drawing.Size(736, 200);
+			this.panelTop.TabIndex = 3;
+			// 
+			// splitButton1
+			// 
+			this.splitButton1.AutoSize = true;
+			this.splitButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.splitButton1.Image = global::DS_Packer.Properties.Resources.save_as;
+			this.splitButton1.Location = new System.Drawing.Point(84, 5);
+			this.splitButton1.Name = "splitButton1";
+			this.splitButton1.Size = new System.Drawing.Size(96, 40);
+			this.splitButton1.SplitMenu = this.contextMenu1;
+			this.splitButton1.TabIndex = 4;
+			this.splitButton1.Text = "Save";
+			this.splitButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.splitButton1.UseVisualStyleBackColor = true;
+			this.splitButton1.Click += new System.EventHandler(this.splitButton1_Click);
+			// 
+			// contextMenu1
+			// 
+			this.contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1,
+            this.menuItem2,
+            this.menuItem3});
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = 0;
+			this.menuItem1.Text = "CSV";
+			// 
+			// menuItem2
+			// 
+			this.menuItem2.Index = 1;
+			this.menuItem2.Text = "DS MAP";
+			// 
+			// menuItem3
+			// 
+			this.menuItem3.Index = 2;
+			this.menuItem3.Text = "DS LIST";
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.button3.Image = global::DS_Packer.Properties.Resources.page_white_database;
+			this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.button3.Location = new System.Drawing.Point(5, 5);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(73, 40);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "New File";
+			this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.button2.Image = global::DS_Packer.Properties.Resources.folder_page;
+			this.button2.Location = new System.Drawing.Point(186, 5);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(77, 40);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Open GDS";
+			this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// btnImportCSV
+			// 
+			this.btnImportCSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.btnImportCSV.Image = global::DS_Packer.Properties.Resources.folder_database;
+			this.btnImportCSV.Location = new System.Drawing.Point(269, 5);
+			this.btnImportCSV.Name = "btnImportCSV";
+			this.btnImportCSV.Size = new System.Drawing.Size(82, 40);
+			this.btnImportCSV.TabIndex = 0;
+			this.btnImportCSV.Text = "Import CSV";
+			this.btnImportCSV.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.btnImportCSV.UseVisualStyleBackColor = true;
+			this.btnImportCSV.Click += new System.EventHandler(this.btnImportCSV_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.SupportMultiDottedExtensions = true;
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.SupportMultiDottedExtensions = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.panel2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.checkBox1);
+			this.groupBox1.Location = new System.Drawing.Point(5, 55);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(728, 145);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Options";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(33, 72);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(80, 17);
+			this.checkBox1.TabIndex = 0;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(7, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(132, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "CSV Import/Export options";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(10, 32);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(85, 17);
+			this.radioButton1.TabIndex = 2;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "radioButton1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.radioButton3);
+			this.panel2.Controls.Add(this.radioButton2);
+			this.panel2.Location = new System.Drawing.Point(167, 32);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(101, 92);
+			this.panel2.TabIndex = 3;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(13, 16);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(85, 17);
+			this.radioButton2.TabIndex = 4;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "radioButton2";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(8, 38);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(85, 17);
+			this.radioButton3.TabIndex = 5;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "radioButton3";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// btnOptions
+			// 
+			this.btnOptions.Location = new System.Drawing.Point(493, 23);
+			this.btnOptions.Name = "btnOptions";
+			this.btnOptions.Size = new System.Drawing.Size(75, 23);
+			this.btnOptions.TabIndex = 6;
+			this.btnOptions.Text = "options";
+			this.btnOptions.UseVisualStyleBackColor = true;
+			this.btnOptions.Click += new System.EventHandler(this.btnOptions_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(736, 433);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.panelTop);
+			this.Controls.Add(this.statusBar1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Shown += new System.EventHandler(this.Form1_Shown);
+			((System.ComponentModel.ISupportInitialize)(this.statusBarPanel1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.statusBarPanel2)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panelTop.ResumeLayout(false);
+			this.panelTop.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader cKey;
+		private System.Windows.Forms.ColumnHeader cValue;
+		private System.Windows.Forms.StatusBar statusBar1;
+		private System.Windows.Forms.StatusBarPanel statusBarPanel1;
+		private System.Windows.Forms.StatusBarPanel statusBarPanel2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panelTop;
+		private System.Windows.Forms.Button btnImportCSV;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private wyDay.Controls.SplitButton splitButton1;
+		private System.Windows.Forms.ContextMenu contextMenu1;
+		private System.Windows.Forms.MenuItem menuItem1;
+		private System.Windows.Forms.MenuItem menuItem2;
+		private System.Windows.Forms.MenuItem menuItem3;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Button btnOptions;
+
+
+	}
+}
+
